df43f0309688a9dd3eefaf8710b21d2bfbc1dbad
Callum Walley <callum.walley@nesi.org.nz>
2025-03-17 18:46:23 +1300

    added example submit script and timings

---
+++ submit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+#SBATCH --account    nesi99991      # Your project code
+#SBATCH --job-name   TILDT          # Name to appear in squeue 
+#SBATCH --time       01:00:00       # Max walltime 
+#SBATCH --cpus-per-task 4
+#SBATCH --mem-per-cpu   2G             # Max memory
+#SBATCH --partition  milan
+
+module load MATLAB/2023b
+
+# Run the MATLAB script MATLAB_job.m 
+matlab -nodisplay < lofi_search_TEE_basic.m
