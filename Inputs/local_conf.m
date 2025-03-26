38742ffd111d5ea47bb86fc69a9645a75f3362c4
hcsen <hcsen@172.24.48.253>
2025-03-27 12:57:53 +1300

    Fixed rng = 1

--- Inputs/local_conf.m
+++ Inputs/local_conf.m
@@ -1,5 +1,5 @@
-rng_seed = 'shuffle'; % Needs to be 'shuffle', or int
-mice_path = {'C:',' Users', 'hcsen', 'Documents', 'MATLAB', 'mice'};   % Path to mice library.
-kernel_path = {'C:',' Users', 'hcsen', 'Documents', 'MATLAB', 'TILTD', 'saturn_ev.tm'};  % Path to kernel
+rng_seed = 1; % Needs to be 'shuffle', or int
+mice_path = {'C:','Users', 'hcsen', 'Documents', 'MATLAB', 'mice'};   % Path to mice library.
+kernel_path = {'C:','Users', 'hcsen', 'Documents', 'MATLAB', 'TILTD', 'Kernels', 'saturn_ev.tm'};  % Path to kernel
 
 use_parallel = false;   % whether to use parpool or not.
