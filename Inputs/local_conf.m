f97d6e1b49902bb8349841fd79c625213ce83b14
hcsen <hcsen@172.24.48.253>
2025-03-27 11:14:48 +1300

    Added Local Config File

---
+++ Inputs/local_conf.m
@@ -0,0 +1,5 @@
+rng_seed = 'shuffle'; % Needs to be 'shuffle', or int
+mice_path = {'C:',' Users', 'hcsen', 'Documents', 'MATLAB', 'mice'};   % Path to mice library.
+kernel_path = {'C:',' Users', 'hcsen', 'Documents', 'MATLAB', 'TILTD', 'saturn_ev.tm'};  % Path to kernel
+
+use_parallel = false;   % whether to use parpool or not.
