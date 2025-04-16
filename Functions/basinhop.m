833287e3c3249562c08555249938acb2390410cd
Callum Walley <callum.walley@nesi.org.nz>
2025-04-16 15:34:07 +1200

    simplify stage logic

--- Functions/basinhop.m
+++ Functions/basinhop.m
@@ -1,4 +1,4 @@
-function [optimised, m_optim, constrviolation] = basinhop(k, optimised, sigmas, MBH_tail, ...
+function [optimised, m_optim, constrviolation] = basinhop(k, best, sigmas, MBH_tail, ...
         MBH_theta, rho_hop, t0Hop, dtHop, dtIndex, lb, ub, ind_vinfi, ind_vinff, s_v, N_flybys, ...
         phaseSizes, objInd, whichThrust, consts, options, A, b, Aeq, beq, Np)
 
@@ -32,17 +32,19 @@ function [optimised, m_optim, constrviolation] = basinhop(k, optimised, sigmas, 
 
 
         % Ensure all variables are local to avoid issues in parfor
-        probSize = length(optimised);
         constsCopy = consts; % Avoid broadcast issues. TERRIBLE
         m_optim = 0; % Initialize m_optim
         output = struct(); % Initialize output as an empty struct
+        
+
+        probSize = length(best);
 
         % Perturb all the decision variables
         pm = fix(rand(1, probSize) + 0.5);
         pm(~pm) = -1;
 
         % TODO: This doesn't need to be done twice.
-        perturbed = optimised + gprnd(MBH_tail, sigmas, MBH_theta * ones(1, probSize)) .* pm;
+        perturbed = best + gprnd(MBH_tail, sigmas, MBH_theta * ones(1, probSize)) .* pm;
 
         % Hop to a new random guess if needed
         if rand < rho_hop
@@ -59,15 +61,13 @@ function [optimised, m_optim, constrviolation] = basinhop(k, optimised, sigmas, 
             if perturbed(j) < lb(j)
                 perturbed(j) = lb(j);
                 % Adjust vinfi if it equals vinff
-                if N_flybys > 0
-                    for vind = 1:N_flybys
-                        if norm(perturbed(ind_vinfi((1:3) + (vind - 1) * 3))) == norm(perturbed(ind_vinff((1:3) + (vind - 1) * 3)))
-                            for f = 1:3
-                                if perturbed(ind_vinfi(f + (vind - 1) * 3)) < 0
-                                    perturbed(ind_vinfi(f + (vind - 1) * 3)) = perturbed(ind_vinfi(f + (vind - 1) * 3)) + s_v * rand;
-                                else
-                                    perturbed(ind_vinfi(f + (vind - 1) * 3)) = perturbed(ind_vinfi(f + (vind - 1) * 3)) - s_v * rand;
-                                end
+                for vind = 1:N_flybys
+                    if norm(perturbed(ind_vinfi((1:3) + (vind - 1) * 3))) == norm(perturbed(ind_vinff((1:3) + (vind - 1) * 3)))
+                        for f = 1:3
+                            if perturbed(ind_vinfi(f + (vind - 1) * 3)) < 0
+                                perturbed(ind_vinfi(f + (vind - 1) * 3)) = perturbed(ind_vinfi(f + (vind - 1) * 3)) + s_v * rand;
+                            else
+                                perturbed(ind_vinfi(f + (vind - 1) * 3)) = perturbed(ind_vinfi(f + (vind - 1) * 3)) - s_v * rand;
                             end
                         end
                     end
@@ -75,15 +75,13 @@ function [optimised, m_optim, constrviolation] = basinhop(k, optimised, sigmas, 
             elseif perturbed(j) > ub(j)
                 perturbed(j) = ub(j);
                 % Adjust vinfi if it equals vinff
-                if N_flybys > 0
-                    for vind = 1:N_flybys
-                        if norm(perturbed(ind_vinfi((1:3) + (vind - 1) * 3))) == norm(perturbed(ind_vinff((1:3) + (vind - 1) * 3)))
-                            for f = 1:3
-                                if perturbed(ind_vinfi(f + (vind - 1) * 3)) < 0
-                                    perturbed(ind_vinfi(f + (vind - 1) * 3)) = perturbed(ind_vinfi(f + (vind - 1) * 3)) + s_v * rand;
-                                else
-                                    perturbed(ind_vinfi(f + (vind - 1) * 3)) = perturbed(ind_vinfi(f + (vind - 1) * 3)) - s_v * rand;
-                                end
+                for vind = 1:N_flybys
+                    if norm(perturbed(ind_vinfi((1:3) + (vind - 1) * 3))) == norm(perturbed(ind_vinff((1:3) + (vind - 1) * 3)))
+                        for f = 1:3
+                            if perturbed(ind_vinfi(f + (vind - 1) * 3)) < 0
+                                perturbed(ind_vinfi(f + (vind - 1) * 3)) = perturbed(ind_vinfi(f + (vind - 1) * 3)) + s_v * rand;
+                            else
+                                perturbed(ind_vinfi(f + (vind - 1) * 3)) = perturbed(ind_vinfi(f + (vind - 1) * 3)) - s_v * rand;
                             end
                         end
                     end
@@ -91,60 +89,20 @@ function [optimised, m_optim, constrviolation] = basinhop(k, optimised, sigmas, 
             end
         end
 
-        % Re-optimize first phase
-        % TODO: See no reason this cant be in the loop as index 1
-        constsCopy(7) = 1;
-        lbCurrent = lb(1:phaseSizes(1));
-        ubCurrent = ub(1:phaseSizes(1));
-        x = perturbed(1:phaseSizes(1));
-        if whichThrust(1) == 1
-            [optimised, ~, ~, ~] = fmincon(@(x)obj_lofiSF(x, objInd(1)), x, A, b, Aeq, beq, lbCurrent, ubCurrent, @(x)con_lofiSF(x, constsCopy), options);
-        else
-            [optimised, ~, ~, ~] = fmincon(@(x)obj_lofiSF_coast(x, objInd(1)), x, A, b, Aeq, beq, lbCurrent, ubCurrent, @(x)con_lofiSF(x, constsCopy), options);
-        end
-        perturbed(1:phaseSizes(1)) = optimised;
-
-        % Re-optimize intermediate phases
-        NpCurrent = 2;
-        for i = 2:Np-1
-            constsCopy(7) = NpCurrent;
+        % Re-optimize phases
+        for i = 1:Np
+            constsCopy(7) = i;
             x = perturbed(1:phaseSizes(i));
             lbCurrent = lb(1:phaseSizes(i));
             ubCurrent = ub(1:phaseSizes(i));
 
-            % TODO: Simplify this
-            if any(i == whichThrust)
-                [optimised, ~, ~, ~] = fmincon(@(x)obj_lofiSF(x, objInd(i)), x, A, b, Aeq, beq, lbCurrent, ubCurrent, @(x)con_lofiSF(x, constsCopy), options);
+            if any(i <= whichThrust)
+                [optimised, ~, ~, output] = fmincon(@(x)obj_lofiSF(x, objInd(i)), x, A, b, Aeq, beq, lbCurrent, ubCurrent, @(x)con_lofiSF(x, constsCopy), options);
             else
-                if any(i <= whichThrust)
-                    [optimised, ~, ~, ~] = fmincon(@(x)obj_lofiSF(x, objInd(i)), x, A, b, Aeq, beq, lbCurrent, ubCurrent, @(x)con_lofiSF(x, constsCopy), options);
-                else
-                    [optimised, ~, ~, ~] = fmincon(@(x)obj_lofiSF_coast(x, objInd(i)), x, A, b, Aeq, beq, lbCurrent, ubCurrent, @(x)con_lofiSF(x, constsCopy), options);
-                end
+                [optimised, ~, ~, output] = fmincon(@(x)obj_lofiSF_coast(x, objInd(i)), x, A, b, Aeq, beq, lbCurrent, ubCurrent, @(x)con_lofiSF(x, constsCopy), options);
             end
             perturbed(1:phaseSizes(i)) = optimised;
-            NpCurrent = NpCurrent + 1;
-        end
-
-        % Re-optimize final phase
-        constsCopy(7) = Np;
-        x = perturbed;
-        % TODO: 
-        % surely `whichThrust(end) == Np` is a subset of `any(whichThrust)`
-        % This can also be put into loop.
-        if whichThrust(end) == Np
-            [optimised, m_optim, ~, output] = fmincon(@(x)obj_lofiSF(x, objInd(Np)), x, A, b, Aeq, beq, lb, ub, @(x)con_lofiSF(x, constsCopy), options);
-        else
-            if any(whichThrust)
-                [optimised, m_optim, ~, output] = fmincon(@(x)obj_lofiSF(x, objInd(Np)), x, A, b, Aeq, beq, lb, ub, @(x)con_lofiSF(x, constsCopy), options);
-            else
-                [optimised, m_optim, ~, output] = fmincon(@(x)obj_lofiSF_coast(x, objInd(Np)), x, A, b, Aeq, beq, lb, ub, @(x)con_lofiSF(x, constsCopy), options);
-            end
         end
         
         constrviolation = output.constrviolation;
-        % Where is optimised compared with
-
-        % TODO: 'optimised', 'm_optim' and 'output' value being returned is just for Final Phase? Is
-        % this correct?
     end
