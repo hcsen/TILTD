d6245e227e72d31cbca2cb74877bf2fe302e9235
Callum Walley <callum.walley@nesi.org.nz>
2025-05-01 10:40:39 +1200

    reduce wait time.

--- Functions/asyncsave.m
+++ Functions/asyncsave.m
@@ -1,6 +1,6 @@
 function asyncsave(root, filename, struct)
     while exist(fullfile(root, '.lock'), 'file') > 0
-        pause(1);
+        pause(0.1);
     end
     save(fullfile(root, '.lock'));
     save(fullfile(root, filename)', '-struct', 'struct', '-mat');
