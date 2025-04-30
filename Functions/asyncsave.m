17ff1a4d9e43250bb0e0f0cda75365d1d7686d53
Callum Walley <callum.walley@nesi.org.nz>
2025-04-30 12:03:01 +1200

    add asyncsave function

---
+++ Functions/asyncsave.m
@@ -0,0 +1,8 @@
+function asyncsave(root, filename, struct)
+    while exist(fullfile(root, '.lock'), 'file') > 0
+        pause(1);
+    end
+    save(fullfile(root, '.lock'));
+    save(fullfile(root, filename)', '-struct', 'struct', '-mat');
+    delete(fullfile(root, '.lock'));
+end
