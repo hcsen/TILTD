a37e180df2d99a804474f93c4e1d0334f734e701
Callum Walley <callum.walley@nesi.org.nz>
2025-06-02 21:43:53 +1200

    fixes

--- Inputs/hpc_conf.m
+++ Inputs/hpc_conf.m
@@ -1,5 +1,5 @@
-rng_seed = 'shuffle'; % Needs to be 'shuffle', or int
+rng_seed = str2num(getenv('SLURM_ARRAY_TASK_ID')); % Needs to be 'shuffle', or int
 mice_path = {'..', 'mice'};   % Path to mice library.
 kernel_path = {'Kernels', 'saturn_ev.tm'};  % Path to kernel
 
-use_parallel = false;   % whether to use parpool or not.
+use_parallel = true;   % whether to use parpool or not.
